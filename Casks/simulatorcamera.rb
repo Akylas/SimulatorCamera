c9003aa170bece92c427864740a2302f4c914f81
Martin Guillon <dev@akylas.fr>
2026-04-28 18:35:14 +0200

    chore: project and icon

--- Casks/simulatorcamera.rb
+++ Casks/simulatorcamera.rb
@@ -2,11 +2,11 @@ cask "simulatorcamera" do
   version "0.2.0"
   sha256 "REPLACE_WITH_ACTUAL_DMG_SHA256_ON_RELEASE"
 
-  url "https://github.com/dautovri/SimulatorCamera/releases/download/v#{version}/SimulatorCamera-#{version}.dmg",
+  url "https://github.com/farfromrefug/SimulatorCamera/releases/download/v#{version}/SimulatorCamera-#{version}.dmg",
       verified: "github.com/dautovri/SimulatorCamera/"
   name "SimulatorCamera"
   desc "Stream a real Mac camera, video file, or screen region into the iOS Simulator"
-  homepage "https://github.com/dautovri/SimulatorCamera"
+  homepage "https://github.com/farfromrefug/SimulatorCamera"
 
   livecheck do
     url :url
